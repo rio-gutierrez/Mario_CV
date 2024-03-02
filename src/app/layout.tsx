c03cb64f34f2f06986f081dfd68dc8126c4cfacc
Mario Gutierrez <gutierrez.m101587@gmail.com>
2024-03-02 13:44:30 +0100

    add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,34 @@
-import { ServerThemeProvider } from "next-themes";
-import Providers from "./providers";
-import { generalInfo } from "@/data/info";
-import "@/styles/globals.css";
+import { ServerThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react'
+import Providers from './providers'
+import { generalInfo } from '@/data/info'
+import '@/styles/globals.css'
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: `${generalInfo.name} - ${generalInfo.jobTitle}`,
-  description: generalInfo.about,
-};
+   title: `${generalInfo.name} - ${generalInfo.jobTitle}`,
+   description: generalInfo.about
+}
 
 export default function RootLayout({
-  children,
+   children
 }: {
-  children: React.ReactNode;
+   children: React.ReactNode
 }) {
-  return (
-    <ServerThemeProvider attribute="class">
-      <html lang="en">
-        <body className={inter.className}>
-          <Providers>{children}</Providers>
-        </body>
-      </html>
-    </ServerThemeProvider>
-  );
+   return (
+      <ServerThemeProvider attribute='class'>
+         <html lang='en'>
+            <body className={inter.className}>
+               <Providers>
+                  {children}
+                  <Analytics />
+               </Providers>
+            </body>
+         </html>
+      </ServerThemeProvider>
+   )
 }
